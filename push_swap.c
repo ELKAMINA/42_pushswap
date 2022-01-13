9b756e255c7537f68131379b7a9f1e2fce2ed699
Amina EL KHATTABI <ael-khat@e2r7p5.clusters.42paris.fr>
2022-01-13 15:55:36 +0100

    parse and check args OK

--- push_swap.c
+++ push_swap.c
@@ -1,5 +1,19 @@
 #include "push_swap.h"
 
+int	check_args(char	*argv[])
+{
+	int	i; 
+
+	i = 1;
+	while (*argv[i])
+	{
+		if	(*argv[i] < 48 || *argv[i] > 57)
+			return (0);
+		i ++;
+	}
+	return (1);
+}
+
 int main (int   argc, char  *argv[])
 {
 	int i;
@@ -9,23 +23,32 @@ int main (int   argc, char  *argv[])
 
 	if (argc < 2)
 		return 0;
-	n = argc - 1;
-	i = 3;
-	head = ft_lstnew((int)ft_atol(argv[1]));
-	current = ft_lstnew((int)ft_atol(argv[2]));
-	head -> next = current;
-	current -> previous = head;
-	while (i <= n)
+	if	(check_args(argv) == 0)
 	{
-		current = ft_lstnew((int)ft_atol(argv[i]));
-		ft_lstadd_back(&head, current);
-		i++;
+		write (2, "Error\n", 6);
+		return 0;
 	}
-	// je recupere et je parse mes arguments
-	while (head != NULL)
-	{ 
-		printf("%d\n", head -> data);
-		head = head -> next;
+	else 
+	{
+		n = argc - 1;
+		i = 3;
+		head = ft_lstnew((int)ft_atol(argv[1]));
+		current = ft_lstnew((int)ft_atol(argv[2]));
+		head -> next = current;
+		current -> previous = head;
+		while (i <= n)
+		{
+			current = ft_lstnew((int)ft_atol(argv[i]));
+			ft_lstadd_back(&head, current);
+			i++;
+		}
+		// je recupere et je parse mes arguments
+		while (head != NULL)
+		{ 
+			printf("%d\n", head -> data);
+			head = head -> next;
+		}
+
 	}
 	return 0 ;
 }

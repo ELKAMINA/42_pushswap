aae8398c7691ba34b8178af450c6b8e667bf2bf5
Amina EL KHATTABI <aminaelk@macbook-pro-de-amina.home>
2022-02-03 12:52:39 +0100

    OK pour création de deux stacks

--- push_swap_utils4.c
+++ push_swap_utils4.c
@@ -51,26 +51,22 @@ void sorting_under_hundred(t_list **head_A, t_list **head_B)
     int median;
     t_list  *bais;
     t_list  *copy;
-    // int size;
     int pos;
     int i;
+    int j;
 
     bais = *head_B;
     i = 1;
+    j = 1;
     median = calc_median(head_A);
-    copy = NULL;
-    // printf("%d", copy->data);
+    copy = *head_A;
     pos = 1;
     while (copy)
     {
-        //copy = *head_A;
-        //printf("[%d]\n", copy->data);
-        //printf("[%d]\n", (*head_A)->next->data);
         if (copy->data < median)
         {
             pos = update_pos(head_A, copy);
-            //printf("{%d}", pos);
-            //printf("{%d}", middle(head_A));
+            printf("{%d}", pos);
             if (pos >= middle(head_A)){
                 while (i < pos)
                 {
@@ -78,7 +74,6 @@ void sorting_under_hundred(t_list **head_A, t_list **head_B)
                     i++;
                 }
                 push_b(head_B, head_A);
-                //copy = *head_A;
             }
             else 
             {
@@ -88,16 +83,14 @@ void sorting_under_hundred(t_list **head_A, t_list **head_B)
                     i++;
                 }
                 push_b(head_B, head_A);
-                //copy = *head_A;
             }
+            copy = *head_A;
+            i = 1;
         }
-        // copy = *head_A;
-        // pos = 1;
-        // else 
-        //     copy = copy->next;
-        // copy = *head_A;
-        // pos = 1;
-        copy = copy->next;
-    }
+        else 
+            copy = copy->next;
+    }     
 }
+
+
 
